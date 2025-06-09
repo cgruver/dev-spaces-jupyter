0dd755e53616c625ad1d04168d204c55eef747c1
Charro Gruver <cgruver@redhat.com>
2025-06-09 13:08:25 +0000

    update

---
+++ init-python-env.sh
@@ -0,0 +1,10 @@
+if [[ ! -d ${PROJECTS_ROOT}/.venv ]]
+then
+   python -m venv --copies ${PROJECTS_ROOT}/.venv
+   source ${PROJECTS_ROOT}/.venv/bin/activate
+   pip install --upgrade pip
+   pip install ipydatagrid
+   pip install pandas
+   pip install jupyterlab
+   pip install ipykernel
+fi

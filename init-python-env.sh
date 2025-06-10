1963ae82504f6712ee68016659f83f52027f31f0
Charro Gruver <cgruver@redhat.com>
2025-06-10 17:19:10 +0000

    fixed project init

--- init-python-env.sh
+++ init-python-env.sh
@@ -1,10 +1,7 @@
 if [[ ! -d ${PROJECTS_ROOT}/.venv ]]
-then
-   python -m venv --copies ${PROJECTS_ROOT}/.venv
-   source ${PROJECTS_ROOT}/.venv/bin/activate
-   pip install --upgrade pip
-   pip install ipydatagrid
-   pip install pandas
-   pip install jupyterlab
-   pip install ipykernel
+then 
+  python -m venv --copies ${PROJECTS_ROOT}/.venv
+  source ${PROJECTS_ROOT}/.venv/bin/activate
+  pip install --upgrade pip
+  pip install ipydatagrid pandas jupyterlab ipykernel  
 fi

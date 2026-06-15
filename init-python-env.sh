c9d645c5efedc1381314f06fb479be533d9ab5f9
cgruver <cgruver@redhat.com>
2026-06-15 12:48:28 +0000

    updated but not verified
    Signed-off-by: cgruver <cgruver@redhat.com>

--- init-python-env.sh
+++ init-python-env.sh
@@ -1,6 +1,6 @@
 if [[ ! -d ${PROJECTS_ROOT}/.venv ]]
 then 
-  python -m venv --copies ${PROJECTS_ROOT}/.venv
+  python3 -m venv --copies ${PROJECTS_ROOT}/.venv
   source ${PROJECTS_ROOT}/.venv/bin/activate
   pip install --upgrade pip
   pip install ipydatagrid pandas jupyterlab ipykernel  
